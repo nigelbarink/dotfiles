f3bc5595987f6f58dbc2df80d3c742ca472bfe77
Nigel <nigelbarink@hotmail.com>
2024-04-09 23:01:57 +0200

    FEAT: Added rofi, i3status and starhip configuration FIX: Deactivate and activate script now put things in the right spot

--- activate.sh
+++ activate.sh
@@ -1,2 +1,8 @@
 #!/bin/zsh
-stow --restow --dotfiles bash alacritty i3 nvim zsh tmux git
+stow --dotfiles bash zsh tmux git
+mkdir -p $HOME/.config/nvim && stow --dotfiles nvim -t $HOME/.config/nvim  
+mkdir -p $HOME/.config/alacritty && stow --dotfiles alacritty  -t $HOME/.config/alacritty
+mkdir -p $HOME/.config/i3 && stow --dotfiles i3 -t $HOME/.config/i3 
+mkdir -p $HOME/.config/i3status && stow --dotfiles i3status -t $HOME/.config/i3status
+mkdir -p $HOME/.config/rofi && stow --dotfiles rofi -t $HOME/.config/rofi
+stow --dotfiles starship -t $HOME/.config
---
+++ deactivate.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+stow -D --dotfiles bash zsh tmux git
+stow -D --dotfiles alacritty -t $HOME/.config/alacritty 
+stow -D --dotfiles nvim  -t $HOME/.config/nvim
+stow -D --dotfiles i3 -t $HOME/.config/i3
+stow -D --dotfiles i3status -t $HOME/.config/i3status
+stow -D --dotfiles rofi -t $HOME/.config/rofi 
+stow -D --dotfiles starship -t $HOME/.config
